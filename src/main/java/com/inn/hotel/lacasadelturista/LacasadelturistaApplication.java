cc945c4c2785a1c0d6659adb0f319e6e7081082b
Jair Milla <jair.milla@neelevat.com>
2023-06-17 00:47:12 -0500

    first commit

---
+++ src/main/java/com/inn/hotel/lacasadelturista/LacasadelturistaApplication.java
@@ -0,0 +1,13 @@
+package com.inn.hotel.lacasadelturista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LacasadelturistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LacasadelturistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inn/hotel/lacasadelturista/LacasadelturistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inn.hotel.lacasadelturista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LacasadelturistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
