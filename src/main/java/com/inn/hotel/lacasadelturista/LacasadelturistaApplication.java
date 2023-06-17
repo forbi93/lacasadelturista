269bef7ea4675c4f5fc3e729a3e7e872b5291667
Jair Milla <jair.milla@neelevat.com>
2023-06-17 00:56:35 -0500

    aaa

--- src/main/java/com/inn/hotel/lacasadelturista/LacasadelturistaApplication.java
+++ src/main/java/com/inn/hotel/lacasadelturista/LacasadelturistaApplication.java
@@ -2,9 +2,18 @@ package com.inn.hotel.lacasadelturista;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LacasadelturistaApplication {
+
+	@GetMapping("/message")
+
+	public String message(){
+		return "Welcome from Java to Azure";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LacasadelturistaApplication.class, args);
